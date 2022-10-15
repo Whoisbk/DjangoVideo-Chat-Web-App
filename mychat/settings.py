3ee2701e6af950b5fdcf301945aa1d97003e33bb
Future <beekayhuncho@gmail.com>
2022-10-15 16:28:46 +0200

    heroku setup

--- mychat/settings.py
+++ mychat/settings.py
@@ -128,7 +128,6 @@ STATICFILES_DIRS =[
 ]
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'static/images')
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
